5016441e96d7fa8750f7bdbd4c43b649f13a592e
Jerry Chai <jchai002@gmail.com>
2015-10-06 10:51:57 -0400

    add mobile button features

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -24,6 +24,30 @@ $("#contact-button").click(function() {
     }, 1000);
 });
 
+$("#stills-button-mobile").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#stills").offset().top-40
+    }, 1000);
+});
+
+$("#videos-button-mobile").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#videos").offset().top-40
+    }, 1000);
+});
+
+$("#about-button-mobile").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top-40
+    }, 1000);
+});
+
+$("#contact-button-mobile").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top-40
+    }, 1000);
+});
+
 });
 
 
