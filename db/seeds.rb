ad11188f432bf0a2d9e0e173bc4ec69213372666
Jerry Chai <jchai002@gmail.com>
2015-10-06 11:29:28 -0400

    add seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
 
-# philly_path="#{Rails.root}/app/assets/images/philly"
-# Dir.foreach(philly_path) do |item|
-#   next if item == '.' or item == '..' or item == ".DS_Store"
-#   PhillyPicture.create(public_key: Cloudinary::Uploader.upload(philly_path+"/"+item)["public_id"])
-# end
+philly_path="#{Rails.root}/app/assets/images/philly"
+Dir.foreach(philly_path) do |item|
+  next if item == '.' or item == '..' or item == ".DS_Store"
+  PhillyPicture.create(public_key: Cloudinary::Uploader.upload(philly_path+"/"+item)["public_id"])
+end
 
-# cait_path="#{Rails.root}/app/assets/images/cait"
-# Dir.foreach(cait_path) do |item|
-#   next if item == '.' or item == '..' or item == ".DS_Store"
-#   CaitPicture.create(public_key: Cloudinary::Uploader.upload(cait_path+"/"+item)["public_id"])
-# end
+cait_path="#{Rails.root}/app/assets/images/cait"
+Dir.foreach(cait_path) do |item|
+  next if item == '.' or item == '..' or item == ".DS_Store"
+  CaitPicture.create(public_key: Cloudinary::Uploader.upload(cait_path+"/"+item)["public_id"])
+end
 
-# zmags_path="#{Rails.root}/app/assets/images/zmags"
-# Dir.foreach(zmags_path) do |item|
-#   next if item == '.' or item == '..' or item == ".DS_Store"
-#   ZmagsPicture.create(public_key: Cloudinary::Uploader.upload(zmags_path+"/"+item)["public_id"])
-# end
+zmags_path="#{Rails.root}/app/assets/images/zmags"
+Dir.foreach(zmags_path) do |item|
+  next if item == '.' or item == '..' or item == ".DS_Store"
+  ZmagsPicture.create(public_key: Cloudinary::Uploader.upload(zmags_path+"/"+item)["public_id"])
+end
 
 # sierraleone_path="#{Rails.root}/app/assets/images/sierraleone"
 # Dir.foreach(sierraleone_path) do |item|
@@ -23,8 +23,8 @@
 #   SierraleonePicture.create(public_key: Cloudinary::Uploader.upload(sierraleone_path+"/"+item)["public_id"])
 # end
 
-paul_path="#{Rails.root}/app/assets/images/paul"
-Dir.foreach(paul_path) do |item|
-  next if item == '.' or item == '..' or item == ".DS_Store"
-  PaulPicture.create(public_key: Cloudinary::Uploader.upload(paul_path+"/"+item)["public_id"])
-end
+# paul_path="#{Rails.root}/app/assets/images/paul"
+# Dir.foreach(paul_path) do |item|
+#   next if item == '.' or item == '..' or item == ".DS_Store"
+#   PaulPicture.create(public_key: Cloudinary::Uploader.upload(paul_path+"/"+item)["public_id"])
+# end
