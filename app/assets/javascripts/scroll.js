5d305c895b756e41b59e63c97cad91fd7e428816
Jerry Chai <jchai002@gmail.com>
2015-10-06 12:43:04 -0400

    add back to top button

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -48,6 +48,20 @@ $("#contact-button-mobile").click(function() {
     }, 1000);
 });
 
+$(window).scroll(function() {
+                if ($(this).scrollTop() > 500) {
+                    $('#go-top').fadeIn(200);
+                } else {
+                    $('#go-top').fadeOut(200);
+                }
+            });
+
+            $('#go-top').click(function(event) {
+                event.preventDefault();
+                
+                $('html, body').animate({scrollTop: 0}, 1000);
+            })
+
 });
 
 
