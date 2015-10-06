4ccb5f40c21f64fc5afdbd5ff0017117f4f52627
Jerry Chai <jchai002@gmail.com>
2015-10-06 05:15:40 -0400

    import work

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -3,15 +3,26 @@ $(document).ready(function() {
 $("#stills-button").click(function() {
     $('html, body').animate({
         scrollTop: $("#stills").offset().top-40
-    }, 500);
+    }, 1000);
 });
 
 $("#videos-button").click(function() {
     $('html, body').animate({
         scrollTop: $("#videos").offset().top-40
-    }, 500);
+    }, 1000);
 });
 
+$("#about-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top-40
+    }, 1000);
+});
+
+$("#contact-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top-40
+    }, 1000);
+});
 
 });
 
